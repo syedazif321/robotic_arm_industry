cad1e772f7ad10dfbe7158bb24b69d3518a68da4
Mohammed Azif <syedazif321@gmail.com>
2025-07-08 17:29:50 +0530

    Add GAZEBO_MODEL_PATH environment variable for custom models in launch file

--- xarm7_bringup/launch/xarm7_bringup.launch.py
+++ xarm7_bringup/launch/xarm7_bringup.launch.py
@@ -1,21 +1,25 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration, EnvironmentVariable
 from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
 
 def generate_launch_description():
     hw_ns = LaunchConfiguration('hw_ns', default='xarm')
 
+    # Add GAZEBO_MODEL_PATH for custom models
+    set_gazebo_model_path = SetEnvironmentVariable(
+        name='GAZEBO_MODEL_PATH',
+        value=EnvironmentVariable('GAZEBO_MODEL_PATH') + ':' +
+            PathJoinSubstitution([FindPackageShare('xarm_gazebo'), 'worlds', 'models'])
+    )
     # Include the xarm_planner launch
     xarm_planner_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution([
-                FindPackageShare('xarm_planner'),
-                'launch',
-                'xarm7_planner_gazebo.launch.py'
-            ])
-        ),
+        PythonLaunchDescriptionSource([
+            FindPackageShare('xarm_planner'),
+            '/launch/xarm7_planner_gazebo.launch.py'
+        ]),
         launch_arguments={
             'hw_ns': hw_ns
         }.items(),
@@ -23,13 +27,10 @@ def generate_launch_description():
 
     # Include the MoveIt Servo Fake launch
     moveit_servo_fake_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution([
-                FindPackageShare('xarm_moveit_servo'),
-                'launch',
-                '_robot_moveit_servo_fake.launch.py'
-            ])
-        ),
+        PythonLaunchDescriptionSource([
+            FindPackageShare('xarm_moveit_servo'),
+            '/launch/_robot_moveit_servo_fake.launch.py'
+        ]),
         launch_arguments={
             'hw_ns': hw_ns
         }.items(),
@@ -41,6 +42,7 @@ def generate_launch_description():
             default_value='xarm',
             description='Hardware namespace for the robot'
         ),
+        set_gazebo_model_path,
         xarm_planner_launch,
         moveit_servo_fake_launch
     ])
